85b6e9ec52d43ebf4a0bd9d305c864a48cb627b7
John Tucker <john.tucker@sharpspring.com>
2018-03-30 08:57:07 -0400

    Fetch

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import 'babel-polyfill';
+import 'whatwg-fetch';
 import './styles.less';
 
 render(
