d027537db6869d1a6acd8e1e04acc54b4e08d27d
John Tucker <john.tucker@sharpspring.com>
2018-03-29 21:07:27 -0400

    Stuff

--- src/components/Async.tsx
+++ src/components/Async.tsx
@@ -1,16 +1,37 @@
 /* tslint:disable-next-line */
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as fromTodos from 'DUCKS/todos';
 
-/*
 interface StateProps {
 }
 interface DispatchProps {
+  fetchTodos(): void;
 }
-*/
-export default () => {
-  return (
+interface ConnectedProps extends StateProps, DispatchProps {
+}
+export class Connected extends Component<ConnectedProps> {
+  componentDidMount() {
+    const { fetchTodos } = this.props;
+    fetchTodos();
+  }
+  render() {
+    return (
       <div>
         <h2>Async</h2>
       </div>
-  );
+    );
+  }
+}
+const mapStateToProps = () => {
+  return ({
+  });
 };
+const mapDispatchToProps = {
+  fetchTodos: fromTodos.fetchTodos,
+};
+export default connect<StateProps, DispatchProps>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Connected);
+
