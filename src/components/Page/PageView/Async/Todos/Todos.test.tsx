c9f35d12d31d242396d055a3decc19fcc1f46cc0
John Tucker <john.tucker@sharpspring.com>
2018-04-07 11:08:40 -0400

    Stuff

--- src/components/Page/PageView/Async/Todos/Todos.test.tsx
+++ src/components/Page/PageView/Async/Todos/Todos.test.tsx
@@ -1,4 +1,3 @@
-import { todoDefault } from 'DUCKS/todos/Todo';
 import Enzyme, { shallow } from 'enzyme';
 import enzymeAdapterReact16 from 'enzyme-adapter-react-16';
 import React from 'react';
@@ -6,13 +5,18 @@ import Todos from './Todos';
 
 Enzyme.configure({ adapter: new enzymeAdapterReact16() });
 
-const getDefaultProps = () => ({
-  todos: [todoDefault],
-});
-
 describe('Todos component', () => {
 
   it('shallow renders without crashing', () => {
+    const todoDefault = {
+      completed: false,
+      id: 0,
+      title: 'title',
+      userID: 0,
+    };
+    const getDefaultProps = () => ({
+      todos: [todoDefault],
+    });
     const { todos } = getDefaultProps();
     shallow((
       <Todos
